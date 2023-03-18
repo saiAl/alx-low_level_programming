09f3e0d71275c225991755cc1a8a17ee94224421
Ali <ojiip@hotmail.com>
2023-03-18 02:24:48 -0700

    Fixed betty style errors for task 0x00_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 /*
- * main - file name puts.c
- * return == 0 
- * */
+ * *main - function that print string of character to stdout
+ * Return: Always 0.
+ */
 
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
