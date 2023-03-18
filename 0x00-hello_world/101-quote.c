6db38bff76a28ee065d1313e88725da8f6fb6830
Ali <ojiip@hotmail.com>
2023-03-18 03:48:06 -0700

    fixed another mispilling in str 0x00_101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
  * main - Entry point
  *
  * Return: Always 1.
- *
  */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 }
