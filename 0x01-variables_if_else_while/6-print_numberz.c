4686f18e7a121a7edda4b478fb98bd93fd5533fc
Ali <ojiip@hotmail.com>
2023-03-19 11:31:50 -0700

    task 0x01_6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function that print numbers
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
