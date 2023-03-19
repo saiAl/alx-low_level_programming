ae4acbd6752256e0abf32c56ebca7a7c8aecfb6a
Ali <ojiip@hotmail.com>
2023-03-19 11:59:14 -0700

    task 0x01_7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function print alphabet backowrds
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	
+	putchar('\n')
+	return (0);
+}
