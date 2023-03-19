1f977b7a99c8b70b146aa0866f5d5b9069ad3e7f
Ali <ojiip@hotmail.com>
2023-03-19 11:22:08 -0700

    task 0x01_5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function print numbers of base 10
+ *
+ * Return: always 0.
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	return (0);
+}
