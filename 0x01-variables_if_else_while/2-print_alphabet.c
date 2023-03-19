394385b73ca4eb73c0206af6b6e2877cd8776ff0
Ali <ojiip@hotmail.com>
2023-03-19 06:56:26 -0700

    Fix betty style, and add increament after putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
  * main - function that print alphabet from a -> z
  *
  * Return: Always 0.
@@ -9,9 +9,11 @@
 int main(void)
 {
 	char alpha = 'a';
+
 	while (alpha < 'z')
 	{
 		putchar(alpha);
+		alpha++
 	}
 
 	return (0);
