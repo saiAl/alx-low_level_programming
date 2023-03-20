05e3dcfdb204cfc7c2e2ee849a46db24f9465279
Ali <ojiip@hotmail.com>
2023-03-20 00:41:10 -0700

    add newline 0x01_9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,17 +12,15 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
+
+		putchar(i + '0');
 		if (i != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			putchar('9');
-		}
 	}
+	putchar('\n');
 
 	return (0);
 }
