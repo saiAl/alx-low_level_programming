000615b9d8eb3ad97ee755ff7f5b032f706781ef
Ali <ojiip@hotmail.com>
2023-03-21 06:51:05 -0700

    task 0x02_1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+
+
+void print_alphabet(void);
+
+int main(void)
+{
+
+	print_alphabet()
+	return (0);
+}
+
+/**
+ * print_alphabet - function that print alphabet
+ *
+ * Return: string of character.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+}
