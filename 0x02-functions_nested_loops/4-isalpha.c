d819f44be0091f5236570d08b63a0d8167900fbe
Ali <ojiip@hotmail.com>
2023-03-21 23:21:50 -0700

    added prototype & task 0x02_4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - function checks the character if it's alphabet.
+ * @c: parameters for the character.
+ * Return: 1 on success.
+ */
+int _isalpha(int c)
+{
+	int c;
+
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
