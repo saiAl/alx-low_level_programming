0d80c160eea158ef095eac13ed930d39dd42a901
Ali <ojiip@hotmail.com>
2023-03-22 13:21:36 -0700

    task 0x02_8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function print the minute of the day.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar( m % 10 + '0');
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
