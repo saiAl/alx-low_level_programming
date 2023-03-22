103cd28707522a4c873259794ea901a5ce50d42f
Ali <ojiip@hotmail.com>
2023-03-21 21:31:49 -0700

    delete main()

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,6 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * Return: Always 0.
- */
-
-
-void print_alphabet(void);
-
-int main(void)
-{
-
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - function that print alphabet
  *
  * Return: string of character.
