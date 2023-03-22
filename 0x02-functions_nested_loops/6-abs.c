d3451da1cb6d1756dbc12f580b760bd0e9934593
Ali <ojiip@hotmail.com>
2023-03-22 01:50:55 -0700

    added prototype and solve task 0x02_6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - function return tha absolute value
+ *
+ * Return: Always n.
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else 
+	{
+		return (n * -1);
+	}
+}
