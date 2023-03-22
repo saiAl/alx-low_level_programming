6f2f82fd4961fcf2329ffa0d52695dc4bbe9691b
Ali <ojiip@hotmail.com>
2023-03-22 01:16:51 -0700

    added prototype of task 0x02_5 and provide solution.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - function checks numbers and return values
+ * @n: parameter of the number
+ *
+ * Return: 1 on success.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_puthcar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+	_putchar('\n');
+}
