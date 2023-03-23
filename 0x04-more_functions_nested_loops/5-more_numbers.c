529e0a6ddb1048194e7da1b81c8407ac24027178
Ali <ojiip@hotmail.com>
2023-03-23 05:28:42 -0700

    task 0x04_5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int i;
+	int j;
+
+
+	n = 0;
+	while (n < 10)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			_putchar(i + '0');
+		}
+		for (j = 10; j <= 14; j++)
+		{
+			_putchar(j / 10 + '0');
+			_puchar(j % 10 + '0');
+		}
+		_putchar('\n')
+		n++
+	}
+}
