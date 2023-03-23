7845ff257df3ee20d1e8bf99f8f3a631d7edeeed
Ali <ojiip@hotmail.com>
2023-03-23 03:17:49 -0700

    task 0x04_2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function return numbers multiplied.
+ * @a: parametr for first number.
+ * @b: parameter for second number.
+ * Return: value of multiplies
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
