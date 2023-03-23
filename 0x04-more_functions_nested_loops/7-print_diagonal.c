7387da73e33f3008ffa3dc24423885b92a26fb72
Ali <ojiip@hotmail.com>
2023-03-23 14:53:08 -0700

    modified task 0x04_6, and provide solution for task 0x04_7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that print diagonal.
+ * @n: parameter
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+        int i;
+
+        if (n <= 0)
+        {
+                _putchar('\n');
+        }
+        else
+        {
+                for (i = 0; i < n; i++)
+                {
+                        _putchar(' ');
+                }
+		_putchar('\\');
+                _putchar('\n');
+        }
+}
+
