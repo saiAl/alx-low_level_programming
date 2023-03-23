5518439ab2a88b69e67f1c2878ad5f0aff7cb6ee
Ali <ojiip@hotmail.com>
2023-03-23 14:57:38 -0700

    fix betty errors and modifed the condations

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,20 +8,20 @@
 
 void print_diagonal(int n)
 {
-        int i;
-
-        if (n <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                for (i = 0; i < n; i++)
-                {
-                        _putchar(' ');
-                }
+	int i;
+	
+	if (n == 0 || n < 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(' ');
+		}
 		_putchar('\\');
-                _putchar('\n');
-        }
+		_putchar('\n');
+	}
 }
 
