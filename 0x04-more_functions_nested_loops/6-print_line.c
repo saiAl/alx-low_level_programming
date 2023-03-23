8268f65839d64284070be31c518cd55aba6a2b7d
Ali <ojiip@hotmail.com>
2023-03-23 14:29:17 -0700

    task 0x04_6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - function that print line.
+ * @n: parameter
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+}
