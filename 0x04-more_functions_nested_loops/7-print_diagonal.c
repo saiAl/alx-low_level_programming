0f2e098d206267225acf99c77724b3aaccad29ca
Ali <ojiip@hotmail.com>
2023-03-24 14:29:05 -0700

    fix the checker problems 0x04_7 0x04_9

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,7 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n == 0 || n < 0)
 	{
@@ -16,9 +17,12 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j <= i - 1; j++)
+			{
+				_putchar(' ');
+			}
 		}
 		_putchar('\\');
 		_putchar('\n');
