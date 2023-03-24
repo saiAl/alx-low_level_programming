7c203e6e50973141820404bd21e0f73e73cb1e1d
Ali <ojiip@hotmail.com>
2023-03-24 15:12:31 -0700

    fix the code 0x04_20

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,13 +19,13 @@ void print_triangle(int size)
 	else
 	{
 
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
 			for (j = size; j > i; j--)
 			{
 				_putchar(' ');
 			}
-			for (k = 0; k < i; k++)
+			for (k = 1; k <= i; k++)
 			{
 				_putchar('#');
 			}
