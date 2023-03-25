ef51cd6a0f0414d6fdc4aa1009809c2fba4c22a8
Ali <ojiip@hotmail.com>
2023-03-25 16:05:20 -0700

    remove elseif statment

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,7 @@
 void print_to_98(int n)
 {
 
-	if (n <= 0)
-	{
-		for (; n <= 98; n++)
-		{
-			printf("%d", n);
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-	else if (n > 98)
+	if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
@@ -30,6 +21,11 @@ void print_to_98(int n)
 	}
 	else
 	{
-		continue;
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 }
