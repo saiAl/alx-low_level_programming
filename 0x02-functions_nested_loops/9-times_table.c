bf841df34280abb148f2b830bf7fdf8fd929e34a
Ali <ojiip@hotmail.com>
2023-03-25 13:12:02 -0700

    task 0x02_9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * times_table - function that print 9 table
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+	int n;
+	int m;
+	int res;
+
+	while (n <= 9)
+	{
+		for(m = 0; m <= 9; m++)
+		{
+			res = m * n;
+			if (res > 9)
+			{
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
+			}
+			else
+			{
+				_putchar(res + '0');
+			}
+			if (m < 9)
+			{
+				_putchar(',');
+				if (res > 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+		n++;
+	}
+}
