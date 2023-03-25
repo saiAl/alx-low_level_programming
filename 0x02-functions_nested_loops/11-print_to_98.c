e6fa75d5391ab73cb89f109c66073b2c6c12f0bd
Ali <ojiip@hotmail.com>
2023-03-25 16:01:28 -0700

    tas k 0x02_11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print numbers to 98.
+ * @n: parameter for number given
+ * Return: Always 0.
+ */
+
+void print_to_98(int n)
+{
+	int n;
+
+	if (n <= 0)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		continue;
+	}
+}
