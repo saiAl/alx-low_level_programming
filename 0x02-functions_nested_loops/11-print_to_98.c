df1b8fbbfdeeda7cdb56366e3cf4762e32c3e4fe
Ali <ojiip@hotmail.com>
2023-03-25 16:25:30 -0700

    Fix the code

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,23 @@
 void print_to_98(int n)
 {
 
-	if (n > 98)
+	if (n >= 98)
 	{
-		for (; n >= 98; n--)
+		if (n == 98)
 		{
 			printf("%d", n);
-			_putchar(',');
-			_putchar(' ');
+		}
+		else
+		{
+			for (; n >= 98; n--)
+			{
+				printf("%d", n);
+				if (n > 98)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 		}
 	}
 	else
@@ -24,8 +34,11 @@ void print_to_98(int n)
 		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			_putchar(',');
-			_putchar(' ');
+			if (n < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 }
