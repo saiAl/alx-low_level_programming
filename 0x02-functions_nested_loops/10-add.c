58e2a4620e19578f90d87b0e220492c63849a6e3
Ali <ojiip@hotmail.com>
2023-03-25 15:02:20 -0700

    fix betty style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,9 @@
 
 /**
  * add - function add two numbers.
- *
- * Return:sum of the two numbers.
+ * @n: parameter for 1st num.
+ * @m parameter for 2nd num.
+ * Return: sum of the two numbers.
  */
 
 int add(int n, int m)
