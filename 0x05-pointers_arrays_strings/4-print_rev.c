174abdf00997ad0d4c0a7f6701c3f5ad78b9f17c
Ali <ojiip@hotmail.com>
2023-03-27 15:51:26 -0700

    solution task 0x05_4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,5 +7,19 @@
 
 void print_rev(char *s)
 {
+	int i;
+	int j;
+	int length;
 
+	length = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length++;
+	}
+
+	for (j = i - 1; length >= 0; j--)
+	{
+		_putchar(*s);
+	}
 }
