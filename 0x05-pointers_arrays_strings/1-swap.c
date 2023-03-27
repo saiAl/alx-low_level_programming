2bb95c3b98233e343c6dfd06313a7872bcd63838
Ali <ojiip@hotmail.com>
2023-03-27 13:11:33 -0700

    task 0x05_1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - function that swap integer.
+ * @a: param for first number.
+ * @b: param for the second number.
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temporary;
+
+	*a = temporary;
+	*a = *b;
+	*b = temporary;
+}
