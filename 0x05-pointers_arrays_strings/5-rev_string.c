41077d2f3d04ad584675572ea60ea7a420bf31f0
Ali <ojiip@hotmail.com>
2023-03-28 05:54:03 -0700

    task 0x05_5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string.
+ * @s: param
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+	int length;
+	
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++
+	}
+
+	for (; length - 1 >= 0; length--)
+	{
+		s[length - 1];
+	}
+
+}
