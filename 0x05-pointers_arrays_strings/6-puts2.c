cf76f4a30cf323ddb14d2310be731cbd558f254a
Ali <ojiip@hotmail.com>
2023-03-28 14:22:39 -0700

    task 0x05_6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - print string
+ * @s: param
+ *  Return: always 0.
+ */
+
+void puts2(char *s)
+{
+	int length;
+	int i;
+
+	for (i = 0; i < length; i += 2)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
