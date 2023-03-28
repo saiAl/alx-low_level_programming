96a429971ea831045b996e3b6ef31038e512f920
Ali <ojiip@hotmail.com>
2023-03-28 15:07:34 -0700

    task 0x05_8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <main.h>
+
+/**
+ * print_array - print array elements to stdout
+ * @n: param number of elements
+ * @a: param pointer
+ *  Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
