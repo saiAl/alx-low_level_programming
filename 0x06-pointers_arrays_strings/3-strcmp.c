d86a9fb3570404a64609203334b60cc483a0c3e8
Ali <ojiip@hotmail.com>
2023-03-30 16:36:28 -0700

    task 0x06_3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - function that compare two strings.
+ * @s1: param
+ * @s2: param
+ * Return: 0 if equal
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		*s1++;
+		*s2++;
+		if (*s1 == *s2)
+		{
+			return (0);
+		}
+		else if (*s1 > *s2)
+		{
+			return (*s2 - *s1);
+		}
+		else
+		{
+			return (*s1 - *s2);
+		}
+	}
+}
