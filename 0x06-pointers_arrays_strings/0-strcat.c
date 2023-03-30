172743a26f6597339ad26f77097ebbb734139f0f
Ali <ojiip@hotmail.com>
2023-03-30 04:52:14 -0700

    task 0x06_0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * _strcat - function concatenates strings
+ * @dest: param 1st string
+ * @src: param 2nd string
+ * Return: new dest string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int destlen;
+	int srclen;
+
+	destlen = strlen(dest);
+	srclen = strlen(src);
+
+	i = 0;
+	while (i <= strclen)
+	{
+		dest[destlen + i] = src[i];
+		i++;
+	}
+
+}
