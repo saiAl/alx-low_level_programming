25e2e5642412663291572090ffc8628b3e4955e0
Ali <ojiip@hotmail.com>
2023-03-30 16:40:14 -0700

    fix 0x06_3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,17 +12,17 @@ int _strcmp(char *s1, char *s2)
 	{
 		*s1++;
 		*s2++;
-		if (*s1 == *s2)
-		{
-			return (0);
-		}
-		else if (*s1 > *s2)
-		{
-			return (*s2 - *s1);
-		}
-		else
-		{
-			return (*s1 - *s2);
-		}
+	}
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else if (*s1 > *s2)
+	{
+		return (*s2 - *s1);
+	}
+	else
+	{
+		return (*s1 - *s2);
 	}
 }
