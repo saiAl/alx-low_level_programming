c7ba10536136770247d65300aa43bd33742c1f3e
Ali <ojiip@hotmail.com>
2023-03-30 13:49:30 -0700

    added another loop 0x06_2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,12 +17,15 @@ char *_strncpy(char *dest, char *src, int n)
 	src_len = strlen(src);
 	i = 0;
 
-	while (i < src_len && i < n)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 
 	return (dest);
 }
