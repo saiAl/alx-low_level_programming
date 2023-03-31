3038b5251f9366decd9af6217d500af564a67992
Ali <ojiip@hotmail.com>
2023-03-31 05:01:57 -0700

    task 0x06_4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverse array
+ * @a: param for array
+ * @n: param for numbers in array
+ * Return: Always 0.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int start;
+	int end;
+
+	end = n - 1;
+
+	for (start = 0; start < end; start++)
+	{
+		temp = a[start];
+		a[start] = a[end];
+		a[end] = a[start];
+		end--;
+	}
+}
