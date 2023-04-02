df7e024ee5e2b691d6f54c5a7610be0e6601d081
Ali <ojiip@hotmail.com>
2023-04-02 03:08:49 -0700

    task 0x05_100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _atoi - function convert strings to integer.
+ * @s: param
+ * Return: integer numbers
+ */
+
+int *_atoi(char *s)
+{
+	int i;
+	int n;
+
+	n = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		n = n * 10 + (s[i] - 48);
+	}
+	return (n);
+}
