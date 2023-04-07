9a4e769e6d245abc9e8161ed9e79ae64bed3e186
Ali <ojiip@hotmail.com>
2023-04-07 02:10:41 -0700

    task 0x0A_0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: count arguments
+ * @argv: string of arguments
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
