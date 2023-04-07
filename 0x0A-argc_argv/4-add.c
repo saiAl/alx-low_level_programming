22dd30d898aeda3dbc1cf3354cf27f4ff9474df5
Ali <ojiip@hotmail.com>
2023-04-07 04:04:08 -0700

    task 0x0A_4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mian - Entry point
+ * @argc: count
+ * @argv: array of args
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+}
