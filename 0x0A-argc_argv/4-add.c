45977d15476fb9bebb4d7c01fc82718a9a3a9627
Ali <ojiip@hotmail.com>
2023-04-07 04:13:02 -0700

    fix 0x0A_4: error statment

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,18 +14,23 @@ int main(int argc, char *argv[])
 	int sum;
 
 	sum = 0;
-
+	i = 1;
 	if (argc == 1)
 	{
 		printf("0\n");
 	}
-	else
+	else if (*argv[i] >= '0' && *argv[i] <= '9')
 	{
-		for (i = 1; i < argc; i++)
+		for (; i < argc; i++)
 		{
 			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 
 	return (0);
