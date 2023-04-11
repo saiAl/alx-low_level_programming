4f8258a65846e8aa3ce5f38bb6df3dea580938e5
Ali <ojiip@hotmail.com>
2023-04-11 16:36:02 -0700

    task 0x0B_0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include "mian.h"
+
+/**
+ * Create_array - function that create an array using malloc.
+ * @c: character
+ * @size: size of the array
+ * Return: NULL or pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	if (size != 0)
+	{
+		arr = malloc(sizeof(char) * size);
+		arr[0] = c;
+		return (arr);
+	}
+	else
+	{
+		return(NULL);
+	}
+
+}
