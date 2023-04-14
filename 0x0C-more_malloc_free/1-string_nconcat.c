f88d468498cebf2d7004487e45602847dde06691
Ali <ojiip@hotmail.com>
2023-04-14 07:08:13 -0700

    0x0C_1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,11 +19,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len2;
 	unsigned int total;
 
-	if (s1 == NULL)
-		s1 = " ";
+	/*if (s1 == NULL)
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";*/
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
