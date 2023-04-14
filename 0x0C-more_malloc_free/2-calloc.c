b6826ae2682d2721f768c149fed3d995a59e7bf6
Ali <ojiip@hotmail.com>
2023-04-13 17:15:12 -0700

    task 0x0C_2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - create array
+ * @nmemb: number of element
+ * @size: size of array
+ * Return: none
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr;
+
+	if (nmemb == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	return (ptr);
+}
