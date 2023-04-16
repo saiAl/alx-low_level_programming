1b913840ee1eadb674b7c6d5d5da345114184dc3
Ali <ojiip@hotmail.com>
2023-04-16 14:31:10 -0700

    fix 0x05_100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -13,28 +13,30 @@ int _atoi(char *s)
 	int sign;
 	unsigned int res;
 
+	res = 0;
 	sign = 1;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 
-		if (s[i] == '-')
-		{
-			sign = -1;
-		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
+			if (s[i - 1] == '-')
+			{
+				sign = -1;
+			}
+
 			for (j = i; s[j] >= 48 && s[j] <= 57; j++)
 			{
 				res = res * 10 + (s[j] - 48);
 			}
 			break;
 		}
-		else
-		{
-			res = 0;
-		}
 	}
-
-	return (res * sign);
+	
+	if (sign == -1)
+	{
+		res *= sign;
+	}
+	return (res);
 }
