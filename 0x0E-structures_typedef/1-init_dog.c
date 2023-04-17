aad22853c61e0c337e65f974637e829dbabecc9e
Ali <ojiip@hotmail.com>
2023-04-17 15:25:45 -0700

    0x0E_1: allocate memory

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,7 +14,12 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (!d)
+	{
+		d = malloc(sizeof(struct));
+
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
