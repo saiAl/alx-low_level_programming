2455ca1df68c097dfe7ef35ab1a91754fb3d924f
Ali <ojiip@hotmail.com>
2023-04-17 14:46:47 -0700

    0x0E_3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,12 @@
  * @age: age of the dog
  * Description: this is a struct for dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
