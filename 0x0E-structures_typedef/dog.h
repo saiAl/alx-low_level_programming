7d6a6a616c31b68948da6d4ffda67714b2c0a659
Ali <ojiip@hotmail.com>
2023-04-17 11:48:42 -0700

    0x0E: fix betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - data type structre 
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ * Description: this is a struct for dog
+ */
 struct dog 
 {
 	char *name;
