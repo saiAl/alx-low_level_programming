379ff3ffc9d13955d77a7375a792d1438c69c6b3
Ali <ojiip@hotmail.com>
2023-04-17 11:50:20 -0700

    0x0E: f-> betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - data type structre 
+ * struct dog - data type structre
  * @name: name of the dog
  * @owner: owner of the dog
  * @age: age of the dog
  * Description: this is a struct for dog
  */
-struct dog 
+struct dog
 {
 	char *name;
 	char *owner;
