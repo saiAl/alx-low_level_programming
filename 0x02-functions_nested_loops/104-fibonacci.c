2054d4a46eef424b0ce97db0dc559a9781eccea6
sai <ojiip95@gmail.com>
2023-06-08 10:25:36 +0200

    0x02: fix task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,33 +1,32 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * mian - Entry point
+ * Return: Always 0.
  */
 
 int main(void)
 {
-        int i;
-        unsigned n1, n2, next;
+	int i;
+	unsigned int n1, n2, next;
 
-        n1 = 1;
-        n2 = 2;
+	n1 = 1;
+	n2 = 2;
 
-        next = n1 + n2;
+	next = n1 + n2;
 
-        printf("%u, %u, ", n1, n2);
+	printf("%u, %u, ", n1, n2);
 
-        for (i = 3; i <= 98; i++)
-        {
-                n1 = n2;
-                n2 = next;
+	for (i = 3; i <= 98; i++)
+	{
+		n1 = n2;
+		n2 = next;
+		next = n1 + n2;
+		if (i < 98)
+			printf("%u, ", next);
+		else
+			printf("%u \n", next);
+	}
 
-                next = n1 + n2;
-
-                if (i < 98)
-                        printf("%u, ", next);
-                else
-                        printf("%u \n", next);
-        }
-
+	return (0);
 }
