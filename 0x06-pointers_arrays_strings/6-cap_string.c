3c8c15a7040e32a687d36b993872b38ed0e586ab
sai <ojiip95@gmail.com>
2023-06-08 09:45:17 +0200

    0x06: task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int is_letter(char c);
+
 /**
  * cap_string - function that captilaize every word.
  * @s: param
@@ -9,12 +11,31 @@
 char *cap_string(char *s)
 {
 	int i;
+	char cap = 'a' - 'A';
 
-	for (i = 1; s[i]; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i - 1] == ' ' || s[i - 1] == '.' || s[i - 1] == ';' || s[i - 1] == ',' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}')
-			if (s[i] >= 'a' && s[i] <= 'z')
-				s[i] -= 32;
+		if (s[i] >= 'a' && s[i] <= 'z' && !is_letter(s[i - 1]))
+		{
+			if (!is_letter(s[i - 1]))
+			{
+				s[i] -= cap;
+			}
+		}
 	}
+
 	return (s);
+}
+
+/**
+ * is_letter - check if the param given is letter
+ * @c: param
+ * Return: 1 if it's a letter, 0 otherwise
+ */
+int is_letter(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
