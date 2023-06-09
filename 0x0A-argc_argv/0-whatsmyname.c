e0f12ac12d4c30325deae80fcb6485100fec1cb1
sai <ojiip95@gmail.com>
2023-06-09 09:30:28 +0200

    0x0A: task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: param
+ * @argv: param
+ * Return: Always 0.
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
