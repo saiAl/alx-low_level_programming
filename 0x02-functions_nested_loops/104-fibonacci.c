7dd499caf626981aa336b092ae69499f7d0f775b
sai <ojiip95@gmail.com>
2023-06-14 16:46:43 +0200

    0x02: solve the checker error, task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
 /**
- * mian - Entry point
+ * main - Entry point
  * Return: Always 0.
  */
 
 int main(void)
 {
 	int i;
-	unsigned int n1, n2, next;
+	unsigned long int n1, n2, next;
 
 	n1 = 1;
 	n2 = 2;
 
 	next = n1 + n2;
 
-	printf("%u, %u, %u, ", n1, n2, next);
+	printf("%lu, %lu, %lu, ", n1, n2, next);
 
-	for (i = 3; i <= 98; i++)
+	for (i = 14; i <= 98; i++)
 	{
 		n1 = n2;
 		n2 = next;
 		next = n1 + n2;
 		if (i < 98)
-			printf("%u, ", next);
+			printf("%lu, ", next);
 		else
-			printf("%u \n", next);
+			printf("%lu \n", next);
 	}
 
 	return (0);
