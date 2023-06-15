9a1fc27ccab9f5fd611d33f3eb0f42edf65b28fe
sai <ojiip95@gmail.com>
2023-06-15 17:48:00 +0200

    0x0C: task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ *
+ *
+ *
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j;
+	char *new_str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	new_str = malloc(sizeof(char) * (strlen(s1) + n + 1));
+
+	if (new_str)
+	{
+		j = 0;
+
+		for (i = 0; s1[i] != '\0'; i++)
+		{
+			new_str[i] = s1[i];
+			j++;
+		}
+		for (i = 0; i < n; i++)
+		{
+			new_str[j] = s2[i];
+			j++;
+		}
+		new_str[j] = '\0'; 
+	}
+	else
+		return (NULL);
+
+	return (new_str);
+}
