46f5663fdec2efb96c9fe65e312330d6aa8bf8e3
sai <ojiip95@gmail.com>
2023-06-15 18:09:28 +0200

    0x0C: fix betty style and betty-doc, task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * malloc_checked - function that allocate memory
+ * @b: the amount of memory to allocate
+ * Return: allocated memory, 98 on falure
  */
 
 void *malloc_checked(unsigned int b)
