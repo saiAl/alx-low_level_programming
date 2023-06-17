9dcdae9e06d6e94975f568b38bbc17c22adfed68
sai <ojiip95@gmail.com>
2023-06-17 10:36:41 +0200

    0x02: task 103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-	long int n1, n2, next, sum;
+	unsigned int n1, n2, next, sum;
 
 
 	n1 = 1;
 	n2 = 2;
 	next = n1 + n2;
 
+	sum = n2;
 	while (next <= 4000000)
 	{
 		n1 = n2;
@@ -24,7 +25,7 @@ int main(void)
 			sum += next;
 	}
 
-	printf("%lu\n", sum);
+	printf("%u\n", sum);
 
 	return (0);
 }
