d6f7d2358287a0e65b748213024a7add73fb0ec6
sai <ojiip95@gmail.com>
2023-06-17 10:04:37 +0200

    0x02: fix betty-style 102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 {
+	int i;
+	long int n1, n2, next;
 
-        int i;
-        long int n1, n2, next;
+	n1 = 1;
+	n2 = 2;
 
-        n1 = 1;
-        n2 = 2;
+	next = n1 + n2;
+	printf("%lu, %lu, %lu, ", n1, n2, next);
 
-        next = n1 + n2;
-        printf("%lu, %lu, %lu ", n1, n2, next);
-        
-        for (i = 5; i < 50; i++)
-        {
-                n1 = n2;
-                n2 = next;
-                next = n1 + n2;
-                
-                if (i < 50)
-                        printf("%lu, ", next);
 
-                else
-                        printf("%lu \n", next);
-        }
-        return (0);
+	for (i = 5; i < 50; i++)
+	{
+		n1 = n2;
+		n2 = next;
+		next = n1 + n2;
+
+		if (i < 50)
+			printf("%lu, ", next);
+		else
+			printf("%lu \n", next);
+	}
+	return (0);
 }
