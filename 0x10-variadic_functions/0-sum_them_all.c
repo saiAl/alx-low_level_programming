96fea98d122b25579b123872f0ee5148fcb480a1
sai <ojiip95@gmail.com>
2023-06-22 07:38:00 +0200

    0x10: add the condation regarding n

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,15 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	unsigned int res = 0;
 
-	va_start(args, n);
+	if (n == 0)
+		return (0);
+	else
+	{
+		va_start(args, n);
+		for (i = 0; i < n; i++)
+			res += va_arg(args, unsigned int);
 
-	for (i = 0; i < n; i++)
-		res += va_arg(args, unsigned int);
-
-	va_end(args);
-	return (res);
+		va_end(args);
+		return (res);
+	}
 }
