ad027d162035a22e8d1628d0e8436adf380bb6dc
sai <ojiip95@gmail.com>
2023-06-22 20:12:07 +0200

    0x10: try to fix the checker, task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-
+	if (args == NULL || n == NULL)
+		return;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
