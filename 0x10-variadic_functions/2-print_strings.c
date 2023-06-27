3948a39ce56513b82280c80a4211aa7313a48afc
saiAI <ojiip95@gmail.com>
2023-06-27 09:24:01 +0200

    0x10: task 2, handles the last check

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,25 +13,31 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *str;
 
 	if (n == 0)
 	{
 		putchar('\n');
 		return;
 	}
+
 	va_start(args, n);
+
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(args, char*);
+		if (str == NULL)
+			str = "(nill)";
 		if (separator)
 		{
 			if (i < (n - 1))
-				printf("%s%s", va_arg(args, char*), separator);
+				printf("%s%s", str, separator);
 			else
-				printf("%s", va_arg(args, char*));
+				printf("%s", str);
 
 		}
 		else
-			printf("%s", va_arg(args, char*));
+			printf("%s", str);
 
 		if (i == (n - 1))
 			putchar('\n');
