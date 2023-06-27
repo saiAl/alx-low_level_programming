98fd84f60c5c522d8ee87ed3d471797726a9ee70
saiAI <ojiip95@gmail.com>
2023-06-27 09:02:35 +0200

    0x10: fix the checker task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,15 +14,21 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
+	if (n == 0)
+	{
+		putchar('\n');
+		return;
+	}
 	va_start(args, n);
 	for (i = 0; i < n; i++)
+	{
 		if (separator)
-		{
-			if (i < n - 1)
-				printf("%s%s", va_arg(args, char*), separator);
-			else
-				printf("%s\n", va_arg(args, char*));
-		}
+			printf("%s%s", va_arg(args, char*), separator);
+		else
+			printf("%s", va_arg(args, char*));
 
+		if (i == (n - 1))
+			putchar('\n');
+	}
 	va_end(args);
 }
