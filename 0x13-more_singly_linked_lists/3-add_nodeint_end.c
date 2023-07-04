b1c757cc650f4245025eac96c74395efd368bdfa
saiAI <ojiip95@gmail.com>
2023-07-04 21:02:46 +0200

    0x13: task 3, fix the betty

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
+ * add_nodeint_end - added data at the end of linked list
+ * @head: double pointer point to the head of the list
+ * @n: new data to added
+ * Return: the new data added
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
@@ -12,13 +13,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new, *last;
 
 	new = (listint_t *) malloc(sizeof(listint_t));
-	
+
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
-	
+
 	last = *head;
 	if (last == NULL)
 		*head = new;
