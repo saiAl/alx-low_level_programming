8ab2b813a27e1e2c447c7630df4b27c6d8c6dd6c
saiAI <ojiip95@gmail.com>
2023-07-11 10:18:57 +0200

    0x15: grant open function the right access mode, task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,15 +18,17 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC | O_APPEND, S_IRUSR | S_IWUSR);
 
 	if (fd == -1)
 		return (-1);
 
+
 	if (text_content)
 	{
 		len = strlen(text_content);
-		write(fd, text_content, len);
+		if ((write(fd, (int *)text_content, len)) == -1)
+			return (-1);
 	}
 	close(fd);
 	return (1);
