3dea0e8033bf9404766d9e33a85333f8800d7f1d
saiAI <ojiip95@gmail.com>
2023-07-11 15:34:18 +0200

    0x15: correct the name, task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	{
 		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to");
 		exit(100);
-	}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
+	}
 
 	from_fd = open(argv[1], O_RDONLY);
 	if (from_fd == -1)
