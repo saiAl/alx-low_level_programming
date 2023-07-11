aec7e94114f0df90b473054145cc58b36b35ca6e
saiAI <ojiip95@gmail.com>
2023-07-11 09:43:01 +0200

    0x15: add the length of the string, task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+
 /**
  * create_file - function that creates a file.
  * @filename: the file to be created
@@ -11,19 +13,21 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len;
 
 	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_TRUNC | O_APPEND, S_IRUSR | S_IWUSR);
-	
+
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
-		write(fd, text_content, 2);
-	
+	{
+		len = strlen(text_content);
+		write(fd, text_content, len);
+	}
 	close(fd);
 	return (1);
 }
