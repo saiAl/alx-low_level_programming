090adb1cf2389cb7c54b802b3577337f3e84d96a
sai <ojiip95@gmail.com>
2023-08-18 21:47:37 +0200

    0x13: task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - function that frees likned list
+ * @head: node
+ * Return: Void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+
+	*head = NULL;
+}
