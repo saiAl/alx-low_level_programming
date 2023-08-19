5f1d3307ffd21ad50b8e0fd660f2715d98a01242
sai <ojiip95@gmail.com>
2023-08-19 16:22:44 +0200

    0x13: task 9, fix the last checker error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,10 +14,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *ptr;
 	unsigned int i = 0;
 
-	(void)idx;
-	if (*head == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
@@ -32,13 +28,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx > i)
 		return (NULL);
-	
+
 	new->n = n;
 	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 	}
-	
+
+
 	return (new);
 }
