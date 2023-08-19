5386c2b687b86eff1b25666cfeb327b26e44a063
sai <ojiip95@gmail.com>
2023-08-19 15:01:36 +0200

    0x13: task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that sum all data in linked list
+ * head: node
+ * Return: sum of data
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
