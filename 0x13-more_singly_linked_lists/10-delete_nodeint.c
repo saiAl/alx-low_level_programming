41070e5f634fac20219d2abe4513c0b48135d9de
sai <ojiip95@gmail.com>
2023-08-20 13:49:44 +0200

    0x13: task 10, handle the index out of range

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,11 +10,11 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *tmp;
+	unsigned int i;
 
-	
 	if ((*head) == NULL)
 		return (-1);
-	
+
 	tmp = *head;
 
 	if (index == 0)
@@ -22,6 +22,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = (*head)->next;
 		tmp->next = NULL;
 		free(tmp);
+	}
+
+	while (tmp != NULL)
+	{
+		i++;
+		tmp = tmp->next;
+		if (index > i)
+			return (-1);
+		if (i == index)
+			break;
 	}
 	return (1);
 }
