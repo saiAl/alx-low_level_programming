6b15a12f52bfcfa178061515586ad7c3815e22f6
sai <ojiip95@gmail.com>
2023-08-20 10:53:20 +0200

    0x13: task 9, handle the NULL case and inserting at the beginning

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
-
+	
 	ptr = *head;
 	current = *head;
 
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		current = current->next;
 
 	new->n = n;
-	if (idx == 0)
+	if (idx == 0 || *head == NULL)
 	{
 		new->next = *head;
 		*head = new;
