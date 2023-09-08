68ec3d1168cd80e5ff67e42fb4a88284b7678397
sai <ojiip95@gmail.com>
2023-09-08 15:44:42 +0200

    0x1A: task 3, 4

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,10 +12,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index;
 	hash_node_t *new;
-	
+
 	if (key == NULL || ht == NULL)
 		return (0);
-	
+
 	new = (hash_node_t *)malloc(sizeof(hash_node_t *));
 	if (!new)
 		return (0);
@@ -23,10 +23,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->key = (char *)key;
 	new->value = (char *)value;
 	new->next = NULL;
+	
 	index = key_index((unsigned char *)key, ht->size);
 
-	if (ht->array[index] != NULL)
-		ht->array[index]->next = new;
 	ht->array[index] = new;
 	return (1);
 }
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table
+ * @key: key to retrieve its value
+ * Return: return value pair with key, otherwise return NULL on failure
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	hash_node_t *new;
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	new = (hash_node_t *)malloc(sizeof(hash_node_t *));
+	if (!new)
+		return (NULL);
+
+	new = ht->array[index];
+	if (ht->array[index] == NULL || new->value == NULL)
+		return (NULL);
+	if (new->next)
+		return new->next->value;
+
+	return new->value;
+}
