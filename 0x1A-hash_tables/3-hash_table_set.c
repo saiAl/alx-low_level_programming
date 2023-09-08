9e6376d2cd12d93c6b8cef03024866148719d61b
sai <ojiip95@gmail.com>
2023-09-08 15:20:04 +0200

    0x1A: task 3, handle the null edge cases

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned int index;
 	hash_node_t *new;
-
+	
+	if (key == NULL || ht == NULL)
+		return (0);
+	
 	new = (hash_node_t *)malloc(sizeof(hash_node_t *));
 	if (!new)
 		return (0);
