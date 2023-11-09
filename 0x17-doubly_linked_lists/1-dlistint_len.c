e9d33b03bc8973eadfb33a172cfb503d63410c85
sai <ojiip95@gmail.com>
2023-11-09 17:34:26 +0200

    0x17: task 1, fix betty docs

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 
 /**
- *
- *
+ * dlistint_len - function that return the size of DLL
+ * @h: the DLL
+ * Return: size of the DLL on Success, NULL on Failure
  */
 
 size_t dlistint_len(const dlistint_t *h)
