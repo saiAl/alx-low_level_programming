92be3ad7f21355c7bff1950b4885ebdd8ea2c864
sai <ojiip95@gmail.com>
2023-11-09 17:31:51 +0200

    0x17: task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,20 +1,18 @@
 #include "lists.h"
 
 /**
- * dlistint_len - function that count all elements
- * @h: the list
- * Return: the elements count
+ *
+ *
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t count = 0;
+	size_t size = 0;
 
 	while (h != NULL)
 	{
-		count++;
 		h = h->next;
+		size++;
 	}
-
-	return (count);
+	return (size);
 }
