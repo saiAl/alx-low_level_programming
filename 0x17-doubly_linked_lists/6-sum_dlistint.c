64abca9a178743e2ed0c60c2537edc8a0dabe621
sai <ojiip95@gmail.com>
2023-11-09 19:25:58 +0200

    0x17: task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
 /**
- * sum_dlistint - function that sum all data in linked list
- * @head: node
- * Return: sum of data on sucess, 0 on failure
+ * sum_dlistint - function that returns the sum of all n
+ * @head: the DLL
+ * Return: the sum on Success, 0 on failure
  */
-
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
