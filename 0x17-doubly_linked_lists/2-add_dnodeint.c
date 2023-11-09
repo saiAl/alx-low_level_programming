b84f88b3913ff13b6e1a36f994f7ac0c4d2c21eb
sai <ojiip95@gmail.com>
2023-11-09 17:54:25 +0200

    0x17: task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,27 +1,29 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * add_dnodeint - function that add node at the beginning
- * @head: the linked list
- * @n: the date to be added
- * Return: new Node
+ * add_dnodeint - function that add a new node aat the beginning of DLL
+ * @head: the DLL
+ * @n: the data
+ * Return: the address of the new element on Success, NULL on Failure
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
-
-	new = malloc(sizeof(dlistint_t));
-
+	
+	new = (dlistint_t *)malloc(sizeof(dlistint_t *));
 	if (!new)
 		return (NULL);
-	new->next = *head;
-	new->prev = NULL;
-	new->n = n;
-	if (*head)
-		(*head)->prev = new;
-	*head = new;
 
+	new->n = n;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		(*head) = new;
+	}
 	return (new);
 }
